efa10fc57852171019f676ee2925d0b79f7706c1
Ondrej89 <ondrejdudas89@gmail.com>
2023-07-17 18:18:43 +0200

    first real

---
+++ src/data/contactsData.js
@@ -0,0 +1,8 @@
+export const contactsData = {
+    email: 'ondrejdudas89@gmail.com',
+    phone: '+381638364945',
+    address: 'Radnička 6, Kulpin - 21472, Serbia',
+    github: 'https://github.com/Ondrej89',
+    facebook: 'https://www.facebook.com/mgrondrej.dudas',
+    linkedIn: 'https://www.linkedin.com/in/ondrej-dudas'
+}
