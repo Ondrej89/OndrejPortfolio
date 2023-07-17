a83f142de5d95d475f1148b53adb74d793399a56
Ondrej89 <ondrejdudas89@gmail.com>
2023-07-17 19:30:49 +0200

    skills

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,23 +1,24 @@
 export const skillsData = [
     'HTML',
-    'Figma',
     'Javascript',
     'CSS',
     'React',
-    'Docker',
     'Next JS',
     'Node JS',
     'Bootstrap',
-    'Go',
     'Typescript',
-    'Figma',
-    'Ubuntu',
     'MongoDB',
     'Tailwind',
     'MySQL',
     'PostgreSQL',
     'MaterialUI',
-    'Firebase'
+    'Firebase',
+    'Photoshop',
+    'AWS',
+    'C#',
+    'ViteJS',
+    'Tensorflow',
+    'Wordpress'
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
