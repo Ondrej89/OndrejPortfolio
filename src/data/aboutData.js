f9dfe4d6a864ede3c1c801f2a7471076cf6a19ae
Ondrej89 <ondrejdudas89@gmail.com>
2023-07-18 16:23:02 +0200

    small tweeks

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name is Ondrej Dudaš. I'm a web designer and developer based in Kulpin, Serbia.",
-    description2: "I am a self-taught and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and I am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, React, NextJs, Material UI, MongoDB, Mysql, AWS, Firebase, Cloudinary, etc.",
+    description2: "I am a self-taught and passionate programmer and AWS certified cloud architect in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and I am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, React, NextJs, Material UI, MongoDB, Mysql, AWS, Firebase, Cloudinary, etc.",
 }
